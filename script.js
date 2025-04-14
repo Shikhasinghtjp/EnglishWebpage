8137fc0ac4d3e03ce7d1c0646a6022aaac13fd61
Shikha Singh <129142909+Shikhasinghtjp@users.noreply.github.com>
2025-04-14 13:50:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.skill').forEach(skill => {
+    skill.addEventListener('mouseover', () => {
+        skill.style.transform = 'scale(1.1)';
+    });
+    skill.addEventListener('mouseout', () => {
+        skill.style.transform = 'scale(1)';
+    });
+});
